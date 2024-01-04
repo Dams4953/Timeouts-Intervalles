f828686ea85ad15de1f157acd2f8de89f3d8e27c
Damien <legat@CQJ>
2024-01-04 11:51:45 +0100

    exo 1

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//EXO 1
+
+/*
+let mot = "wilson-2";
+let i = 0;
+
+
+
+function MachineEcrire() {
+    let temps = setInterval(function() {
+        if (i < mot.length) { 
+            let div = document.getElementById('compte_a_rebours');      
+            div.innerText = mot.substring(0, i + 1);
+            i++;
+            
+        } else {
+            clearInterval(temps)
+        }
+        
+    }, 1000); 
+}
+
+MachineEcrire();
+*/
+
+
+
+
